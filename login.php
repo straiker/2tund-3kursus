53e13803cb7dc97a9ef19c41e26425806ad78722
raitavastu <ravastu@tlu.ee>
2015-09-09 13:29:45 +0300

    tunni viimane

--- login.php
+++ login.php
@@ -1,17 +1,22 @@
 <?php
-    if($_SERVER['REQUEST_METHOD'] == "post"){
+    if($_SERVER['REQUEST_METHOD'] == "POST"){
         $error = "";
+        $email_error = "";
+        $pswd_error = "";
 
-        if(empty($_POST['username']) || empty($_POST['password'])){
-            $username = $_POST['username'];
-            $password = $_POST['password'];
-            echo $username;
-            echo $password;
-        }else{
-            $error = "empty";
+        if(empty($_POST['username'])){
+            $email_error = "Email kohustuslik!";
         }
 
-        echo $error;
+        if (empty($_POST['password'])){
+            $pswd_error = "Password kohustuslik!";
+        }
+
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        echo $username;
+        echo $password;
+
     }
 ?>
 
@@ -19,8 +24,8 @@
     <head></head>
     <body>
         <form action="login.php" method="post">
-            <input type="email" name="username" id="username" placeholder="Email">
-            <input type="password" name="password" id="password" placeholder="password">
+            <input type="email" name="username" id="username" placeholder="Email"><? echo $email_error;?>
+            <input type="password" name="password" id="password" placeholder="password"><? echo $pswd_error;?>
             <input type="submit" name="login" id="login" value="Login">
         </form>
     </body>
