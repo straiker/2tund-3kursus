8d15ab7751f161a29da24089289edaad757488e1
raitavastu <ravastu@tlu.ee>
2015-09-09 11:37:50 +0300

    update 2

--- index.php
+++ index.php
@@ -3,4 +3,14 @@
     $last_name = "Avastu";
 
     echo $first_name." ".$last_name;
+
+    echo "<br>";
+
+    $age = 25;
+
+    if ($age < 18) {
+    	echo "alaealin";
+    } else {
+    	echo "täisealine";
+    }
 ?>
